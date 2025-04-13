cf3f5315b9b17e84c745a89d098e1d44855fc38c
sturgis <sturgis.steele@outlook.com>
2025-04-13 01:05:08 -0500

    update root to / instead of /website

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website/', // Base path for GitHub Pages deployment
+  base: '/', // Base path for GitHub Pages deployment
 })
