4b91290c8fbae7c31be4eb91b0c1e354e3928f1d
sturgis <sturgis.steele@outlook.com>
2025-04-13 02:18:09 -0500

    change files for gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website', // Base path for GitHub Pages deployment - matches the repository name
-})
+  base: '/website/',
+});
